035681429462cf99b432092157b6788f7a023586
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2023-08-14 23:35:49 +0700

    5.1

--- real-world-vue-02/src/views/passenger/PassengerEditView.vue
+++ real-world-vue-02/src/views/passenger/PassengerEditView.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import { type PassengerItem } from '@/type'
 import type { PropType } from 'vue'
+import { useRouter } from 'vue-router';
+import { useMessageStore } from '@/stores/message';
 
-defineProps({
+const props = defineProps({
   passenger: {
     type: Object as PropType<PassengerItem>,
     require: true
   }
 })
+const router = useRouter()
+const store = useMessageStore()
+function edit() {
+  store.updateMessage('You are successfully edited for ' + props.passenger?.first_name)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({
+    name: 'passenger-detail',
+    params: {
+      id: props.passenger?.id
+    }
+  })
+}
 </script>
 <template>
-  <p>Edit the event here</p>
+  <p>Edit the passenger-detail here</p>
+  <button @click="edit">Edit Me</button>
 </template>
